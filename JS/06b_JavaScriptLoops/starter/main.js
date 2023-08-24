e3cdc902a5f179b50d945b6681bac3763543b9fa
Jacob Jewitt <jewittwork@gmail.com>
2023-08-24 14:35:39 +0100

    update

---
+++ JS/06b_JavaScriptLoops/starter/main.js
@@ -0,0 +1,11 @@
+for(let i = 1; i <10 ; i++) {
+    console.log(i);
+}
+
+let x = 2;
+let loopCounter = 0;
+while(x < 10000){
+    loopCounter++;
+    x = x**2;
+    console.log('X = ' + x + ' Loop Counter = ' + loopCounter);
+}
